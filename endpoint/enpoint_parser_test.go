1161bb312f81c00a23fb54271665584fae99dab8
van muscari <vamuscari@gmail.com>
2025-06-13 11:49:33 -0400

    format input for sharing

--- endpoint/enpoint_parser_test.go
+++ endpoint/enpoint_parser_test.go
@@ -8,7 +8,155 @@ import (
 
 func TestParseJSON(t *testing.T) {
 
-	const inputJSON string = `
+	const expectedJSON string = `
+[
+  {
+    "name": "Customers",
+    "tableName": "Customers",
+    "schemaName": "dbo",
+    "joins": [
+      {
+        "endpoint": "Invoices",
+        "on": [
+          "CustomerID",
+          "CustomerID"
+        ]
+      }
+    ],
+    "fields": [
+      {
+        "name": "Active",
+        "type": "BOOLEAN",
+        "nullable": true
+      },
+      {
+        "name": "CreateDate",
+        "type": "DATE",
+        "nullable": true
+      },
+      {
+        "name": "CustomerID",
+        "type": "STRING",
+        "nullable": false
+      },
+      {
+        "name": "FirstName",
+        "type": "STRING",
+        "nullable": true
+      },
+      {
+        "name": "LastName",
+        "type": "STRING",
+        "nullable": true
+      },
+      {
+        "name": "Zip",
+        "type": "INTEGER",
+        "nullable": true
+      }
+    ]
+  },
+  {
+    "name": "Invoices",
+    "tableName": "Invoices",
+    "schemaName": "dbo",
+    "joins": [
+      {
+        "endpoint": "Customers",
+        "on": [
+          "CustomerID",
+          "CustomerID"
+        ]
+      }
+    ],
+    "fields": [
+      {
+        "name": "Balance",
+        "type": "FLOAT",
+        "nullable": true
+      },
+      {
+        "name": "CreateDate",
+        "type": "DATE",
+        "nullable": true
+      },
+      {
+        "name": "CustomerID",
+        "type": "STRING",
+        "nullable": false
+      },
+      {
+        "name": "InvoiceNumber",
+        "type": "INTEGER",
+        "nullable": true
+      }
+    ]
+  }
+]
+`
+
+	service, err := ParseJSON([]byte(testingJSON()))
+	if err != nil {
+		t.Fatalf(`error: %v`, err)
+	}
+
+	// paths := service.AllEndpointsPaths(2)
+	// for _, p := range paths {
+	// 	fmt.Println(strings.Join(p, "/"))
+	// }
+
+	expected_endpoints := []string{"Customers", "Invoices"}
+	for _, expected_endpoint := range expected_endpoints {
+		_, ok := service.Endpoints[expected_endpoint]
+		if !ok {
+			t.Errorf("Enpoint error for %s", expected_endpoint)
+		}
+	}
+
+	comparableJSON := strings.Replace(expectedJSON, "\n", "", -1)
+	comparableJSON = strings.Replace(comparableJSON, "\t", "", -1)
+	comparableJSON = strings.Replace(comparableJSON, " ", "", -1)
+
+	evaluatedJSON := strings.Replace(service.JSON(), " ", "", -1)
+
+	if comparableJSON != evaluatedJSON {
+		t.Errorf("evaluated JSON did not match expected JSON\n\nexpected:\n%s\n\nevaluated:\n%s\n\n", comparableJSON, evaluatedJSON)
+	}
+
+}
+
+func diffStrings(str1, str2 string) {
+	var matchLen int
+	if len(str1) > len(str2) {
+		matchLen = len(str2)
+	} else {
+		matchLen = len(str1)
+	}
+	matchBreak := true
+	matched := ""
+	diff1 := ""
+	diff2 := ""
+
+	for i := range matchLen {
+		if matchBreak && str1[i] == str2[i] {
+			matched = matched + string(str1[i])
+		} else {
+			matchBreak = false
+			diff1 = diff1 + string(str1[i])
+			diff2 = diff2 + string(str2[i])
+		}
+	}
+
+	fmt.Println("Matched JSON")
+	fmt.Println(matched)
+	fmt.Println("Expected JSON Diff")
+	fmt.Println(diff1)
+	fmt.Println("Evaluated JSON Diff")
+	fmt.Println(diff2)
+}
+
+func testingJSON() string {
+	return `
 [
   {
     "name": "Customers",
@@ -109,150 +257,4 @@ func TestParseJSON(t *testing.T) {
 
 ]
 `
-
-	const expectedJSON string = `
-[
-  {
-    "name": "Customers",
-    "tableName": "Customers",
-    "schemaName": "dbo",
-    "joins": [
-      {
-        "endpoint": "Invoices",
-        "on": [
-          "CustomerID",
-          "CustomerID"
-        ]
-      }
-    ],
-    "fields": [
-      {
-        "name": "Active",
-        "type": "BOOLEAN",
-        "nullable": true
-      },
-      {
-        "name": "CreateDate",
-        "type": "DATE",
-        "nullable": true
-      },
-      {
-        "name": "CustomerID",
-        "type": "STRING",
-        "nullable": false
-      },
-      {
-        "name": "FirstName",
-        "type": "STRING",
-        "nullable": true
-      },
-      {
-        "name": "LastName",
-        "type": "STRING",
-        "nullable": true
-      },
-      {
-        "name": "Zip",
-        "type": "INTEGER",
-        "nullable": true
-      }
-    ]
-  },
-  {
-    "name": "Invoices",
-    "tableName": "Invoices",
-    "schemaName": "dbo",
-    "joins": [
-      {
-        "endpoint": "Customers",
-        "on": [
-          "CustomerID",
-          "CustomerID"
-        ]
-      }
-    ],
-    "fields": [
-      {
-        "name": "Balance",
-        "type": "FLOAT",
-        "nullable": true
-      },
-      {
-        "name": "CreateDate",
-        "type": "DATE",
-        "nullable": true
-      },
-      {
-        "name": "CustomerID",
-        "type": "STRING",
-        "nullable": false
-      },
-      {
-        "name": "InvoiceNumber",
-        "type": "INTEGER",
-        "nullable": true
-      }
-    ]
-  }
-]
-`
-
-	service, err := ParseJSON([]byte(inputJSON))
-	if err != nil {
-		t.Fatalf(`error: %v`, err)
-	}
-
-	// paths := service.AllEndpointsPaths(2)
-	// for _, p := range paths {
-	// 	fmt.Println(strings.Join(p, "/"))
-	// }
-
-	expected_endpoints := []string{"Customers", "Invoices"}
-	for _, expected_endpoint := range expected_endpoints {
-		_, ok := service.Endpoints[expected_endpoint]
-		if !ok {
-			t.Errorf("Enpoint error for %s", expected_endpoint)
-		}
-	}
-
-	comparableJSON := strings.Replace(expectedJSON, "\n", "", -1)
-	comparableJSON = strings.Replace(comparableJSON, "\t", "", -1)
-	comparableJSON = strings.Replace(comparableJSON, " ", "", -1)
-
-	evaluatedJSON := strings.Replace(service.JSON(), " ", "", -1)
-
-	if comparableJSON != evaluatedJSON {
-		t.Errorf("evaluated JSON did not match expected JSON\n\nexpected:\n%s\n\nevaluated:\n%s\n\n", comparableJSON, evaluatedJSON)
-	}
-
-}
-
-func diffStrings(str1, str2 string) {
-	var matchLen int
-	if len(str1) > len(str2) {
-		matchLen = len(str2)
-	} else {
-		matchLen = len(str1)
-	}
-	matchBreak := true
-	matched := ""
-	diff1 := ""
-	diff2 := ""
-
-	for i := range matchLen {
-		if matchBreak && str1[i] == str2[i] {
-			matched = matched + string(str1[i])
-		} else {
-			matchBreak = false
-			diff1 = diff1 + string(str1[i])
-			diff2 = diff2 + string(str2[i])
-		}
-	}
-
-	fmt.Println("Matched JSON")
-	fmt.Println(matched)
-	fmt.Println("Expected JSON Diff")
-	fmt.Println(diff1)
-	fmt.Println("Evaluated JSON Diff")
-	fmt.Println(diff2)
 }
