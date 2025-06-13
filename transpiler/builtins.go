baac6e4aa21c9bef07ce8830c9f4216f8a158a2f
van muscari <vamuscari@gmail.com>
2025-06-13 14:06:45 -0400

    Add like builtin

--- transpiler/builtins.go
+++ transpiler/builtins.go
@@ -38,6 +38,21 @@ var builtins = map[string]func(ir *IR, args ...object.Object) object.Object{
 			return newError("Invalid Type. %s %s", arg.Type(), arg.String())
 		}
 	},
+	"datetime": func(ir *IR, args ...object.Object) object.Object {
+		if len(args) != 1 {
+			return newError("wrong number of arguments. got=%d, want=1", len(args))
+		}
+
+		arg := args[0]
+
+		switch {
+		case arg.Type() == object.STRING_OBJ:
+			return &object.Expression{ExpressionType: object.DATE_OBJ,
+				Value: fmt.Sprintf("CONVERT(date, %s, 23)", args[0])}
+		default:
+			return newError("Invalid Type. %s %s", arg.Type(), arg.String())
+		}
+	},
 	"exclude": func(ir *IR, args ...object.Object) object.Object {
 		if len(args) != 1 {
 			return newError("wrong number of arguments. got=%d, want=1", len(args))
@@ -51,6 +66,21 @@ var builtins = map[string]func(ir *IR, args ...object.Object) object.Object{
 			return &object.Builtin{}
 		default:
 			return newError("Invalid function call for exclude. %s %s", arg.Type(), arg.String())
+		}
+	},
+	"like": func(ir *IR, args ...object.Object) object.Object {
+		if len(args) != 1 {
+			return newError("wrong number of arguments. got=%d, want=1", len(args))
+		}
+
+		arg := args[0]
+
+		switch {
+		case arg.Type() == object.STRING_OBJ:
+			return &object.Expression{ExpressionType: object.BOOLEAN_OBJ,
+				Value: fmt.Sprintf("LIKE %s", args[0])}
+		default:
+			return newError("Invalid Type. %s %s", arg.Type(), arg.String())
 		}
 	},
 }
