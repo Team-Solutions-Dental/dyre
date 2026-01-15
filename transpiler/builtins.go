f4de30c53926315a5a081a479abc3a33d6dfc325
noah.b <noah.b@tsdlab.com>
2026-01-15 08:26:01 -0500

    Forgot comma

--- transpiler/builtins.go
+++ transpiler/builtins.go
@@ -33,7 +33,7 @@ var builtins = map[string]func(ir *IR, local *objectRef.LocalReferences, args ..
 		arg := args[0]
 
 		return &object.Expression{Value: fmt.Sprintf("DISTINCT %s", args[0])}	
-	}
+	},
 	//cast(expression, to)
 	"cast": func(ir *IR, local *objectRef.LocalReferences, args ...object.Object) object.Object {
 		if len(args) != 2 {
